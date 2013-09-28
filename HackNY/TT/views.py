bd8ac75550f004925af828ef4133b003152cbe2d
Abhi Agarwal <me@abhiagarwal.com>
2013-09-28 18:14:08 -0400

    Adding

--- HackNY/TT/views.py
+++ HackNY/TT/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+import json
+
+def index(request):
+	now = datetime.datetime.now()
+	html = "<html><head></head><body>hello %s</body></html>" %now
+	return HttpResponse(html)
+
+def news(request):
+	
