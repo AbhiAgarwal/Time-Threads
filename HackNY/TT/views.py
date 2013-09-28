9bd9de03358b87f1c468e467cd295d2d9cc5eed2
Abhi Agarwal <me@abhiagarwal.com>
2013-09-28 18:22:39 -0400

    Adding

--- HackNY/TT/views.py
+++ HackNY/TT/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 import datetime
 import json
+import urllib2
+
+articleSearch = "895b5c2b2d01446ed289e27f8efadd91:6:67424427"
 
 def index(request):
 	now = datetime.datetime.now()
-	html = "<html><head></head><body>hello %s</body></html>" %now
+	html = "<html><head></head><body>hello %s</body></html>" % now
 	return HttpResponse(html)
 
 def news(request):
-	
+	response = urllib2.urlopen('https://api.instagram.com/v1/tags/pizza/media/XXXXXX')
