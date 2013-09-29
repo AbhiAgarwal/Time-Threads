19cf79b6efe72d0af1b6544736866f112ccda04d
Abhi Agarwal <me@abhiagarwal.com>
2013-09-29 00:56:05 -0400

    Rename Django

--- HackNY/JSON/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from JSON import views
-
-urlpatterns = patterns('',
-    url(r'^news/$', views.news, name='news')
-)
--- HackNY/TT/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from TT import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
-)
