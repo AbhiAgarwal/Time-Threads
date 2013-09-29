94f8276ee0b9f91b51bad5d6990c944f201e940b
Abhi Agarwal <me@abhiagarwal.com>
2013-09-29 16:47:19 -0400

    Adding WSGI

--- Flask/make.py
+++ Flask/make.py
@@ -3,14 +3,9 @@ from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
 from main import app
 
-class MainHandler(RequestHandler):
-  def get(self):
-    self.write("This message comes from Tornado ^_^")
-
 tr = WSGIContainer(app)
 
 application = Application([
-(r"/tornado", MainHandler),
 (r".*", FallbackHandler, dict(fallback=tr)),
 ])
 
